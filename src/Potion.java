9fdc015fb5e05c5bec29fe7e8bbaac7834e67781
0IIIIM0 <ignac23med@gmail.com>
2022-04-04 14:02:45 -0500

    new class Character, Pokemon, Trainer.

--- src/Potion.java
+++ src/Potion.java
@@ -1,8 +1,13 @@
 public class Potion extends Item{
+    public Potion(double price, int quantity, String name, String type) {
+        super(price, quantity, name, type);
+    }
+
     // effect -String
     //duration -int
 
-
+    private String efecto;
+    private int duracion;
     //use -
         //healing
             //increase 20% health
