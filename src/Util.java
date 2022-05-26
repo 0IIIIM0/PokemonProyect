be64e86ebe84fc6ca13c2f238421ebbab4bc7545
0IIIIM0 <ignac23med@gmail.com>
2022-05-26 11:30:35 -0500

    create Interface

---
+++ src/Util.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public interface Util {
+    void timer(long initialTime,long finalTime);
+    void displayMenu();
+    Pokemon createPokemonAleatory();
+    <G> G takeoutaleatory(ArrayList<G>metothauxarray);
+    String[] weaknessAndStreightaleatory(String type);
+    String[] aleatorynames(String type);
+
+
+}
